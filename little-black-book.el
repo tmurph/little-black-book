ca150970f27f469aa880b276fefc14aacef2fda5
Trevor Murphy <trevor.m.murphy@gmail.com>
2017-01-11 11:50:26 -0800

    Include GPL3 license in file.

--- little-black-book.el
+++ little-black-book.el
@@ -1,4 +1,25 @@
 ;;; little-black-book --- Add my names to Org Capture Templates
+
+;; Copyright (C) 2017  Trevor Murphy
+
+;; Author: Trevor Murphy <trevor.m.murphy@gmail.com>
+;; Maintainer: Trevor Murphy <trevor.m.murphy@gmail.com>
+
+;; This file is not part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;;; I'm trying to keep a little black book on everybody.  It's tough
