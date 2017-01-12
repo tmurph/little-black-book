00d203170e9c758f7cf5940fe035fa4d2239c792
Trevor Murphy <trevor.m.murphy@gmail.com>
2017-01-11 17:16:17 -0800

    Unit test the capture output function.

--- little-black-book.el
+++ little-black-book.el
@@ -194,6 +194,24 @@
            (lbb-build-table-of-inputs (current-buffer)))))
     (should (hash-table-equal expected-table actual-table))))
 
+(ert-deftest lbb-test-format-one-capture ()
+  "Test formatting just one capture template."
+  (let* ((expected-list
+          `((,black-book-capture-prefix "People")
+            (,(concat black-book-capture-prefix "j") "People - Js")
+            (,(concat black-book-capture-prefix "jm")
+             "Joseph Marquez" entry
+             (file+headline ,black-book-file "Joseph Marquez")
+             "* %^{Title|A Quick Obs}\n%U\n\n%?")))
+         (actual-list
+          (lbb-build-list-of-captures
+           #s(hash-table
+              test equal
+              data ("j" #s(hash-table
+                           test equal
+                           data ("jm" ("Joseph Marquez"))))))))
+    (should (equal expected-list actual-list))))
+
 (ert-deftest lbb-test-build-two-inputs ()
   "Test parsing of two org headlines into the hash table format."
   (let ((expected-table
@@ -213,6 +231,32 @@
            (lbb-build-table-of-inputs (current-buffer)))))
     (should (hash-table-equal expected-table actual-table))))
 
+(ert-deftest lbb-test-format-two-captures ()
+  "Test formatting two capture templates."
+  (let* ((expected-list
+          `((,black-book-capture-prefix "People")
+            (,(concat black-book-capture-prefix "a") "People - As")
+            (,(concat black-book-capture-prefix "al")
+             "Adam Lindgren" entry
+             (file+headline ,black-book-file "Adam Lindgren")
+             "* %^{Title|A Quick Obs}\n%U\n\n%?")
+            (,(concat black-book-capture-prefix "j") "People - Js")
+            (,(concat black-book-capture-prefix "jm")
+             "Joseph Marquez" entry
+             (file+headline ,black-book-file "Joseph Marquez")
+             "* %^{Title|A Quick Obs}\n%U\n\n%?")))
+         (actual-list
+          (lbb-build-list-of-captures
+           #s(hash-table
+              test equal
+              data ("j" #s(hash-table
+                           test equal
+                           data ("jm" ("Joseph Marquez")))
+                    "a" #s(hash-table
+                           test equal
+                           data ("al" ("Adam Lindgren"))))))))
+    (should (equal expected-list actual-list))))
+
 (ert-deftest lbb-test-build-two-inputs-same-initials ()
   "Test parsing of two org headlines into the hash table format."
   (let ((expected-table
@@ -228,6 +272,30 @@
            (org-mode)
            (lbb-build-table-of-inputs (current-buffer)))))
     (should (hash-table-equal expected-table actual-table))))
+
+(ert-deftest lbb-test-format-two-captures-same-initials ()
+  "Test formatting two capture templates."
+  (let* ((expected-list
+          `((,black-book-capture-prefix "People")
+            (,(concat black-book-capture-prefix "j") "People - Js")
+            (,(concat black-book-capture-prefix "jm") "People - All JMs")
+            (,(concat black-book-capture-prefix "jm2")
+             "Joseph Marquez" entry
+             (file+headline ,black-book-file "Joseph Marquez")
+             "* %^{Title|A Quick Obs}\n%U\n\n%?")
+            (,(concat black-book-capture-prefix "jm1")
+             "Just Mango" entry
+             (file+headline ,black-book-file "Just Mango")
+             "* %^{Title|A Quick Obs}\n%U\n\n%?")
+            ))
+         (actual-list
+          (lbb-build-list-of-captures
+           #s(hash-table
+              test equal
+              data ("j" #s(hash-table
+                           test equal
+                           data ("jm" ("Just Mango" "Joseph Marquez"))))))))
+    (should (equal expected-list actual-list))))
 
 (provide 'little-black-book)
 ;;; little-black-book.el ends here
